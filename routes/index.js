13db2f48319a79093ca7160d3e1fcec74dec51ea
DImhoff79 <david.imhoff@gmail.com>
2017-11-06 21:30:43 -0500

    added url parser and a test route under trucks/test

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var url = require('url');
+var headers = Array;
 var router = express.Router();
+var u = url;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +11,14 @@ router.get('/', function(req, res, next) {
 
 router.get('/trucks', function(req,res,next){
 res.send("This is the test endpoint, it doesnt use a view");
+});
+
+router.get('/trucks/test', function(req,res,next){
+res.render('test', {title: 'Dave is cool'});
+
+res.send(u.parse(req.originalUrl));
+
+
 });
 
 module.exports = router;
